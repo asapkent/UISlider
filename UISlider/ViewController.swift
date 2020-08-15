842c254fdf3d2c0c9ab67fac7c656a57a58a401c
asapkent <jeffersalex94@gmail.com>
2020-08-15 12:37:11 -0400

    Initial Commit

---
+++ UISlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by Robert Jeffers on 8/15/20.
+//  Copyright © 2020 AsapInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
